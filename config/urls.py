37e14418e5dd54eb1be5fa64758ee68a953e411c
Ahmed <testuser@Ahmed.Ahmed>
2026-08-24 18:12:09 +0300

    Connect to database via DATABASE_URL

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('core/', include('core.urls')),
+    path('', lambda request: redirect('accounts/login/')),
 ]
