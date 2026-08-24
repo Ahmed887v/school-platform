4bccd7c14685dff56a699efa6151c08d2cd7b150
Ahmed <testuser@Ahmed.Ahmed>
2026-08-24 16:30:51 +0300

    Fix model clash and static directory

--- config/settings.py
+++ config/settings.py
@@ -67,7 +67,7 @@ ENCRYPTION_KEY = b'Base64-Encoded-Fernet-Key-Here-1234567890abcdef'
 
 # ملفات ثابتة
 STATIC_URL = '/static/'
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [BASE_DIR / 'static'] if os.path.exists(BASE_DIR / 'static') else []
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
